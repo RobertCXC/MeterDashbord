b21db9a9357cf14df0f50f264cfc5a0c8402c4fa
MMG <53985815+RobertCXC@users.noreply.github.com>
2024-04-02 15:28:31 +0800

    后端添加根据时区读取数据的功能，前端进行适配

--- js/summary.js
+++ js/summary.js
@@ -66,26 +66,41 @@ ajaxGet('http://127.0.0.1:4070/allCurrentPlans', function (error, response) {
   if (error) {
     console.error(error);
   } else {
-    // 获取 <ul> 元素
-    const radioList = document.getElementById('radioList');
-
-
-    // 示例数据
-    const data = response;
-
-    // 生成 HTML 字符串
-    const html = data.map(item => `
-    <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
-    <div class="flex items-center ps-3">
-       <input id="${item.PIndex}" type="radio" value="" name="list-radio"
-          class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500  focus:ring-2 ">
-       <label for="${item.PIndex}"
-          class="w-full py-3 ms-2 text-sm font-medium text-gray-900">${item.PInfo}
-       </label>
-    </div>
-    </li>
-    `).join('');
+    console.log(response)
     
-    radioList.innerHTML = html;
+    myChart.setOption({
+      series:        response.mySeries,
+      xAxis: {
+        data: response.time
+      },
+    });
   }
 });
+
+// ajaxGet('http://127.0.0.1:4070/allCurrentPlans', function (error, response) {
+//   if (error) {
+//     console.error(error);
+//   } else {
+//     // 获取 <ul> 元素
+//     const radioList = document.getElementById('radioList');
+
+
+//     // 示例数据
+//     const data = response;
+
+//     // 生成 HTML 字符串
+//     const html = data.map(item => `
+//     <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+//     <div class="flex items-center ps-3">
+//        <input id="${item.PIndex}" type="radio" value="" name="list-radio"
+//           class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500  focus:ring-2 ">
+//        <label for="${item.PIndex}"
+//           class="w-full py-3 ms-2 text-sm font-medium text-gray-900">${item.PInfo}
+//        </label>
+//     </div>
+//     </li>
+//     `).join('');
+    
+//     radioList.innerHTML = html;
+//   }
+// });
